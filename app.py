346bc9a58ace0be2fbe33bc8199d1412f08f21cb
ollondo <danielamaralpereira2@gmail.com>
2023-11-22 23:14:26 -0300

    Cometeu

--- app.py
+++ app.py
@@ -10,12 +10,17 @@ def index():
 
 @app.route('/processar_formulario', methods=['POST'])
 def processar_formulario():
-    dados_do_formulario = request.get_json()
-    # Lógica para processar os dados do formulário aqui
-    print(dados_do_formulario)
-    
-    # Exemplo: Retornar uma resposta em JSON
-    return jsonify({'mensagem': 'Formulário recebido com sucesso!'})
+    try:
+        dados_do_formulario = request.get_json()
+        # Lógica para processar os dados do formulário aqui
+        print(dados_do_formulario)
+        
+        # Exemplo: Retornar uma resposta em JSON
+        return jsonify({'mensagem': 'Formulário recebido com sucesso!'})
+
+    except Exception as e:
+        # Se ocorrer um erro, retorna uma mensagem de erro
+        return jsonify({'erro': str(e)}), 500  # 500 é o código de status para erro interno do servidor
 
 if __name__ == '__main__':
     app.run(debug=True)
